a2fa92ca1d90c63e145e60848b983dad5e197253
Slava Lysjuk <vlysjuk@gmail.com>
2024-11-04 22:14:20 +0200

    Delete some no need file

---
+++ src/HomeWork_7_1.java
@@ -0,0 +1,13 @@
+public class HomeWork_7_1 {
+
+    public String lastString(String str, int n) {
+        String result = "";
+
+        if (str.length() >= 3) {
+            for (int i = 0; i < n; i++) {
+                result += str.substring(str.length() - n, 5);
+            }
+        }
+        return result;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        HomeWork_7_1 homeWork7_1 = new HomeWork_7_1();
+        System.out.println(homeWork7_1.lastString("Hello", 3));
+
+    }
+
+}
