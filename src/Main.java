5636d68952267f137100805d43254f5aad8d58a2
Slava Lysjuk <vlysjuk@gmail.com>
2024-11-04 23:17:14 +0200

    add HomeWork7_2.java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        HomeWork_7_1 homeWork7_1 = new HomeWork_7_1();
-        System.out.println(homeWork7_1.lastString("Hello", 3));
+//        HomeWork_7_1 homeWork7_1 = new HomeWork_7_1();
+//        System.out.println(homeWork7_1.lastString("Hello", 3));
+
+        HomeWork7_2 homeWork7_2 = new HomeWork7_2();
+        System.out.println(homeWork7_2.concatStr("abc", "xyz"));
+        System.out.println(homeWork7_2.concatStr("Hi", "There"));
+        System.out.println(homeWork7_2.concatStr("xxxx", "There"));
 
     }
 
