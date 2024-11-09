92f02eece1933d7043d9ca27a6a4204d8d466323
Slava Lysjuk <vlysjuk@gmail.com>
2024-11-09 11:49:28 +0200

    add HomeWork7_3.java

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        HomeWork_7_1 homeWork7_1 = new HomeWork_7_1();
-        System.out.println(homeWork7_1.lastString("Hello", 3));
-        System.out.println(homeWork7_1.lastString("Hello", 2));
-        System.out.println(homeWork7_1.lastString("Hello", 1));
-        System.out.println();
+//        HomeWork_7_1 homeWork7_1 = new HomeWork_7_1();
+//        System.out.println(homeWork7_1.lastString("Hello", 3));
+//        System.out.println(homeWork7_1.lastString("Hello", 2));
+//        System.out.println(homeWork7_1.lastString("Hello", 1));
+//        System.out.println();
+//
+//        HomeWork7_2 homeWork7_2 = new HomeWork7_2();
+//        System.out.println(homeWork7_2.concatStr("abc", "xyz"));
+//        System.out.println(homeWork7_2.concatStr("Hi", "There"));
+//        System.out.println(homeWork7_2.concatStr("xxxx", "There"));
 
-        HomeWork7_2 homeWork7_2 = new HomeWork7_2();
-        System.out.println(homeWork7_2.concatStr("abc", "xyz"));
-        System.out.println(homeWork7_2.concatStr("Hi", "There"));
-        System.out.println(homeWork7_2.concatStr("xxxx", "There"));
+        HomeWork7_3 homeWork7_3 = new HomeWork7_3();
+        System.out.println(homeWork7_3.xyzMiddle("AAxyzBB"));
+        System.out.println(homeWork7_3.xyzMiddle("AxyzBB"));
+        System.out.println(homeWork7_3.xyzMiddle("AxyzBBB"));
 
     }
 
