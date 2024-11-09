243f9660384252b1a4bf77754936182b14f41bf4
Slava Lysjuk <vlysjuk@gmail.com>
2024-11-09 12:08:26 +0200

    add HomeWork7_4.java 1

--- src/HomeWork7_4.java
+++ src/HomeWork7_4.java
@@ -4,14 +4,14 @@ public class HomeWork7_4 {
         StringBuilder result = new StringBuilder();
 
         for (int i = 0; i < str.length(); i++) {
-            // Перевіряємо шаблон "z*p", де * може бути будь-яким символом
+
             if (i + 2 < str.length() && str.charAt(i) == 'z' && str.charAt(i + 2) == 'p') {
-                // Додаємо перший і третій символи "zp"
+
                 result.append("zp");
-                // Пропускаємо середній символ
+
                 i += 2;
             } else {
-                // Якщо шаблон не відповідає, додаємо поточний символ
+
                 result.append(str.charAt(i));
             }
         }
