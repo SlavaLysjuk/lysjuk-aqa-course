9802010c8a2ea8a828aad3c6129550b9301057e8
Slava Lysjuk <vlysjuk@gmail.com>
2024-12-01 14:34:47 +0200

    add HomeWork13_2.java.

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,12 @@ public class Main {
         System.out.println(homeWork13_1.getLengthOfStrings(new String[]{"a", "bb", "a", "bb"}));
         System.out.println(homeWork13_1.getLengthOfStrings(new String[]{"this", "and", "that", "and"}));
         System.out.println(homeWork13_1.getLengthOfStrings(new String[]{"code", "code", "code", "bug"}));
+        System.out.println();
+
+        HomeWork13_2 homeWork13_2 = new HomeWork13_2();
+        System.out.println(homeWork13_2.createMapChars(new String[]{"code", "bug"}));
+        System.out.println(homeWork13_2.createMapChars(new String[]{"man", "moon", "main"}));
+        System.out.println(homeWork13_2.createMapChars(new String[]{"man", "moon", "good", "night"}));
     }
 
 
