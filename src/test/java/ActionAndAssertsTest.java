516ef335711bf788cb5c3a0df5413c4f9d823110
Slava Lysjuk <vlysjuk@gmail.com>
2024-12-24 20:58:13 +0200

    Add HomeWork21, new test class for actions

--- src/test/java/ActionAndAssertsTest.java
+++ src/test/java/ActionAndAssertsTest.java
@@ -3,13 +3,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
 public class ActionAndAssertsTest {
 
@@ -26,6 +29,11 @@ public class ActionAndAssertsTest {
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.get("https://guest:welcome2qauto@qauto.forstudy.space/");
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        driver.quit();
     }
 
     @Test(description = "Тест-кейс 1: Перевірка відображення логотипу")
@@ -46,4 +54,27 @@ public class ActionAndAssertsTest {
                 .isEqualTo("rgba(2, 117, 216, 1)")
                 .as("Background color of Sign up button is incorrect");
     }
+
+    @Test(description = "Приклад роботи методу Actions")
+    public void testGuestLoginAndLogOutToMainPageTest() {
+        WebElement guestLogIn = wait.until(visibilityOfElementLocated(By.xpath("//button[text()='Guest log in']")));
+        assertThat(guestLogIn.isDisplayed()).as("Кнопку Guest log in не знайдено");
+        actions.moveToElement(guestLogIn).click().perform();
+
+        wait.until(ExpectedConditions.urlToBe("https://guest:welcome2qauto@qauto.forstudy.space/panel/garage"));
+        String currentUrl = driver.getCurrentUrl();
+        assertThat(currentUrl)
+                .isEqualTo("https://guest:welcome2qauto@qauto.forstudy.space/panel/garage")
+                .as("Некоректний URL");
+
+        WebElement logOutButton = wait.until(visibilityOfElementLocated(By.xpath("//a[text()=' Log out ']")));
+        actions.moveToElement(logOutButton).click().perform();
+
+        wait.until(ExpectedConditions.urlToBe("https://guest:welcome2qauto@qauto.forstudy.space/"));
+        currentUrl = driver.getCurrentUrl();
+        assertThat(currentUrl)
+                .isEqualTo("https://guest:welcome2qauto@qauto.forstudy.space/")
+                .as("Некоректний URL або не вийшли по кнопці Log out");
+    }
+
 }
