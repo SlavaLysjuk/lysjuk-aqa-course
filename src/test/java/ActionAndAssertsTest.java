cc3963aaa0795b474ab4aded1f4a170816a800a1
Slava Lysjuk <vlysjuk@gmail.com>
2024-12-24 20:03:49 +0200

    Add HomeWork21

---
+++ src/test/java/ActionAndAssertsTest.java
@@ -0,0 +1,49 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ActionAndAssertsTest {
+
+    WebDriver driver;
+    WebDriverWait wait;
+    Actions actions;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        actions = new Actions(driver);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://guest:welcome2qauto@qauto.forstudy.space/");
+    }
+
+    @Test(description = "Тест-кейс 1: Перевірка відображення логотипу")
+    public void testFindLogo() {
+        String currentUrl = driver.getCurrentUrl();
+
+        assertThat(currentUrl)
+                .isEqualTo("https://guest:welcome2qauto@qauto.forstudy.space/")
+                .as("Некоректний URL");
+    }
+
+    @Test(description = "Тест-кейс 2: Перевірка кольору фону кнопки \"Sign up\"")
+    public void testCheckButtonSignUpBackgroundColor() {
+        WebElement signUpButton = driver.findElement(By.xpath("//button[text()='Sign up']"));
+        String backgroundColor = signUpButton.getCssValue("color");
+
+        assertThat(backgroundColor)
+                .isEqualTo("rgba(2, 117, 216, 1)")
+                .as("Background color of Sign up button is incorrect");
+    }
+}
